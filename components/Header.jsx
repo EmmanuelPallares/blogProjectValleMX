a1f7984c4d61b8fa7f0a82383ce7282407afbeee
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2021-11-13 17:17:02 -0600

    bug fix finally

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from "react";
 
+import Head from "next/head";
 import Link from "next/link";
 import { getCategories } from "../services";
 
-const Header = () => {
+const Header = ({ post }) => {
+  const [value, setValue] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setValue(new Date()), 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
@@ -13,25 +24,69 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
-        <div className="md:float-left block">
-          <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">
-              Graph CMS
-            </span>
+    <div className="bg-white shadow-lg rounded-lg lg:p-6 pb-12 mb-6">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <nav className="navbar navbar-expand-lg  background-milenio-nav  text-black ">
+        <div className="container">
+          <Link className="navbar-brand" href="/">
+            <h1 className="h1-titulo ">VALLE DE SANTIAGO BLOG oficial</h1>
           </Link>
+          <hr />
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse flex lg:justify-end  "
+            id="navbarNav"
+          >
+            <ul className="navbar-nav  ">
+              {categories.map((category, index) => (
+                <li className="nav-item background-milenio-nav  ">
+                  <a
+                    className="nav-link text-black"
+                    aria-current="page"
+                    href={`/category/${category.slug}`}
+                  >
+                    {category.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className="hidden md:float-left md:contents">
-          {categories.map((category, index) => (
-            <Link key={index} href={`/category/${category.slug}`}>
-              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
-                {category.name}
-              </span>
-            </Link>
-          ))}
+        <div className="nav-scroller py-1 mb-2"></div>
+      </nav>
+      {/* <div className="bg-green-600 mb-4">
+        <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between flex-wrap">
+            <div className="w-0 flex-1 flex items-end">
+              <p className="ml-3 font-medium text-white truncate">
+                <div className="text-center mt-1 padding-0">
+                  <p>Hora Actual</p>
+                </div>
+                <span className="md:hidden">Bienvenido</span>
+              </p>
+            </div>
+
+            <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3"></div>
+          </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
