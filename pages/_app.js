ae31fe211f27b1fe1d0aaf908fbb676612dee2aa
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2021-11-17 12:04:01 -0600

    header tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "../styles/globals.scss";
+import "../styles/globals.css";
 import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }) {
