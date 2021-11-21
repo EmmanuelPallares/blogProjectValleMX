de9c52a846c9d028fd7b867b3b5936fb1bcce655
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2021-11-21 14:53:52 -0600

    adding animated h1

---
+++ components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="text-center mb-8">
+        <p className=" mt-4 text-3xl leading-8 font-extrabold tracking-tight text-gray-700 sm:text-4xl animate-bounce text-center">
+          Historias
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
